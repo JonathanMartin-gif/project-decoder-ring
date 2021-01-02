dddee61a17d1f78ad778f6d65de8d1d961e2ca48
JonathanMartin-gif <74462621+JonathanMartin-gif@users.noreply.github.com>
2021-01-02 15:35:44 -0600

    tests are done

--- tests/polybius.test.js
+++ tests/polybius.test.js
@@ -3,25 +3,39 @@ let chai = require('chai'), expect = chai.expect, should = chai.should(), assert
 
 
 describe('polybius', () => {
-  it('When encoding, it translates the letters i and j to 42',() =>{
-  
+  it('When encoding, it translates the letter i to 42',() =>{
+  const expected = 42;
+  const actual = polybius('i', encode = true);
+  expect(actual).to.equal(expected);
   })
-  it('When decoding, it translates 42 to (i/j)', () =>{
-
+  it('When encoding, it translates the letter j to 42', () => {
+  const expected = 42;
+  const actual = polybius('j', encode = true);
+  expect(actual).to.equal(expected);
+  })
+  it('When decoding, it translates 42 to i', () =>{
+  const expected = 'i';
+  const actual = polybius(42 , encode = true);
+  expect(actual).to.equal(expected)
+  })
+  it('When decoding, it translates 42 to j', ()=>{
+  const expected = 'j';
+  const actual = polybius(42 , encode = true);
+  expect(actual).to.equal(expected)
   })
   it('ignores capital letters', () => {
   const expected = 'thinkful';
-  const actual = polybius('ThInKfUlL', 3, encode = false);
+  const actual = polybius('ThInKfUlL', encode = true);
   expect(actual).to.include(expected);
   })
   it('maintains spaces and other nonalphabetic symbols in the message, before and after encoding', () => {
   const expected = 'cheud pdjdclqh';
-  const actual = polybius('Zebra Magazine', 3, encode = true);
+  const actual = polybius('Zebra Magazine', encode = true);
   expect(actual).to.equal(expected);
   })
   it('maintains spaces and other nonalphabetic symbols in the message, before and after decoding', () => {
    const expected = 'zebra magazine';
-   const actual = polybius('cheud pdjdclqh', 3, encode = false);
+   const actual = polybius('cheud pdjdclqh', encode = true);
    expect(actual).to.equal(expected);
   })
 })
--- tests/substitution.js
+++ tests/substitution.js
@@ -4,27 +4,31 @@ let chai = require('chai'), expect = chai.expect, should = chai.should(), assert
 
 describe('substitution', () => {
   it(`It returns false if the given alphabet isn't exactly 26 characters long`, ()=>{
-
+  const actual = substitution(input, ['a','b'], encode = true);
+  expect(actual).to.be.false;
   })
   it('It correctly translates the given phrase, based on the alphabet given to the function', () =>{
-
+  const expected = 'zebra magazine';
+  const actual = substitution('cheud pdjdclqh', ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], encode = true);
+  expect(actual).to.equal(expected);
   })
   it('It returns false if there are any duplicate characters in the given alphabet', ()=>{
-
+  const actual = substitution('aa', ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], encode = false);
+  expect(actual).to.be.false;
   })
   it('ignores capital letters', () => {
   const expected = 'thinkful';
-  const actual = substitution('ThInKfUlL', 3, encode = false);
+  const actual = substitution('ThInKfUlL', ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], encode = true);
   expect(actual).to.include(expected);
   })
   it('maintains spaces and other nonalphabetic symbols in the message, before and after encoding', () => {
   const expected = 'cheud pdjdclqh';
-  const actual = substitution('Zebra Magazine', 3, encode = true);
+  const actual = substitution('Zebra Magazine', ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], encode = true);
   expect(actual).to.equal(expected);
   })
   it('maintains spaces and other nonalphabetic symbols in the message, before and after decoding', () => {
    const expected = 'zebra magazine';
-   const actual = substitution('cheud pdjdclqh', 3, encode = false);
+   const actual = substitution('cheud pdjdclqh', ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], encode = true);
    expect(actual).to.equal(expected);
   })
 })
