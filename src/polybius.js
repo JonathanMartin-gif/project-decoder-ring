1a8d375b2bcc460ef238a727903a32aa4e5f09f8
JonathanMartin-gif <74462621+JonathanMartin-gif@users.noreply.github.com>
2021-01-05 09:01:25 -0600

    fixed error

--- src/polybius.js
+++ src/polybius.js
@@ -5,13 +5,15 @@ function getCol(letter) {
     // For letters "j" and "k", subtract the index value by one, so "i" and "j" are the same value, and k takes "j"'s value, keeping to the grid
     if(letter === "j" || letter === "k"){
     letterVal--
-  } 
+  }
+  // If the letter's index value is less than or equal to ten, take the modulus of five and add by 1. For index values above ten, check if the the modulus of five equals zero, if so change to five, otherwise return the modulus of five.
   let col = letterVal <= 10 ? (letterVal%5 + 1) : (letterVal%5 === 0 ? 5 : letterVal%5)
   return col
 }
 function getRow(letter){
     // Get the index value of the letter
     const letterVal = alphabet.indexOf(letter)
+    // Using the letter's index value, check if the modulus of five is zero, if so return the rounded down quotient of five, otherwise if there is a remainder, add one to the quotient
     let row = letterVal%5 === 0 ? Math.floor(letterVal/5) : Math.floor(letterVal/5) + 1
     if(letterVal === 0 || letterVal === 5) row += 1
     return row
@@ -27,6 +29,7 @@ function polybius(input, encode = true) {
     // create an empty variable for the resulting input
     let result = ""
     if (encode) {
+        // If the message is being encoded, check each character in the input. If the character is a space, return a space, otherwise return the two-digit code for the letter
         for(characters in input) {
             let character = input[characters]
             result+= character === " " ? character : getNum(character)
@@ -47,6 +50,8 @@ function polybius(input, encode = true) {
                 result = false
                 break
             }
+            
+            // While the word hasn't been completely dissolved, isolate the first two numbers, if the numbers equal 42, return i/j, otherwise go through the alphabet and find out which letter it relates to. Delete this number pairing after solving and add the letter to the resulting string
             while (word.length > 0) {
                 let wordSection = word.substr(0,2)
                 if (wordSection == 42) {
@@ -55,9 +60,11 @@ function polybius(input, encode = true) {
                     continue
                 }
                 for(letters in alphabet){
+                    // Sort through each letter in the alphabet, obtain its 2-digit code, and compare
                     let letter = getNum(alphabet[letters])
                     // Once a match is found, add the letter to the resulting
                     if(letter == wordSection) result += alphabet[letters]
+                    
                 }
                 word = word.slice(2)
             }
@@ -67,5 +74,4 @@ function polybius(input, encode = true) {
     }
     return result
 }
-
 module.exports = polybius;
